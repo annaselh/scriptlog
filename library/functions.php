79ca0970f34276c794c5634c593db3d06d1b8afa
cakmoel <alanmoehammad@gmail.com>
2018-02-18 20:20:34 +0700

    update functions.php

--- library/functions.php
+++ library/functions.php
@@ -9,25 +9,6 @@ function checkDetailRequest($action, $param)
     
 }
 
-// current URL
-function currentUrl()
-{
-    $protocol = strpos(strtolower($_SERVER['SERVER_PROTOCOL']),'https') === FALSE ? 'http' : 'https';
-    $host     = $_SERVER['HTTP_HOST'];
-    $script   = $_SERVER['SCRIPT_NAME'];
-    $params   = $_SERVER['QUERY_STRING'];
-    return $protocol . '://' . $host . $script . '/' . $params;
-}
-
-function currentUri()
-{
-    $basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/';
-    $uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
-    if (strstr($uri, '?')) $uri = substr($uri, 0, strpos($uri, '?'));
-    $uri = '/' . trim($uri, '/');
-    return $uri;
-}
-
 // sanitize header email by Kevin Waterson
 function safeEmail($string)
 {
