46801feef52d88de51e4ba37c164388e70b08fce
cakmoel <alanmoehammad@gmail.com>
2018-05-31 19:51:14 +0700

    AppException.php -100DaysOfCode in Ramadhan

---
+++ library/core/AppException.php
@@ -0,0 +1,15 @@
+<?php if (!defined('SCRIPTLOG')) die("Direct Access Not Allowed!");
+/**
+ * AppException Class extends Exception
+ *
+ * @package   SCRIPTLOG
+ * @author    Maoelana Noermoehammad
+ * @license   MIT
+ * @version   1.0
+ * @since     Since Release 1.0
+ *
+ */
+class AppException extends Exception
+{
+    
+}
