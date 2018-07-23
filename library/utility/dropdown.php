614a3b8fad4c87fa0b2641693d5c6fa04757e884
cakmoel <alanmoehammad@gmail.com>
2018-07-23 17:28:54 +0700

    append portable dropdown utility

---
+++ library/utility/dropdown.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Dropdown Function
+ * 
+ * @link  https://www.phpro.org/tutorials/Dropdown-Select-With-PHP-and-MySQL.html
+ * @param string $name
+ * @param array $options
+ * @param string $selected
+ * @return string
+ */
+function dropdown($name, array $options, $selected=null)
+{
+    $dropdown = '<select class="form-control" name="'.$name.'" id="'.$name.'">'."\n";
+    
+    $selected = $selected;
+    /*** loop over the options ***/
+    foreach( $options as $key=>$option )
+    {
+        /*** assign a selected value ***/
+        $select = $selected==$key ? ' selected' : null;
+        
+        /*** add each option to the dropdown ***/
+        $dropdown .= '<option value="'.$option.'"'.$select.'>'.$option.'</option>'."\n";
+        
+    }
+    
+    /*** close the select ***/
+    $dropdown .= '</select>'."\n";
+    
+    /*** and return the completed dropdown ***/
+    return $dropdown;
+    
+}
