81b9a0a1ae8f63be9a5aa3f782840e297a8f565c
cakmoel <alanmoehammad@gmail.com>
2019-02-13 17:08:15 +0700

    refactoring media management #301DaysOfCode

--- lib/event/MediaEvent.php
+++ lib/event/MediaEvent.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Class MediaEvent
+ * 
  * 
  */
 class MediaEvent
@@ -141,36 +143,80 @@ class MediaEvent
    $this->media_caption = $caption;
  }
 
+/**
+ * Set media type
+ * 
+ * @param string $type
+ * 
+ */
  public function setMediaType($type)
  {
    $this->media_type = $type;
  }
 
+/**
+ * Set media target
+ * 
+ * @param string $target
+ * 
+ */
  public function setMediaTarget($target)
  {
    $this->media_target = $target;
  }
 
+/**
+ * Set media user
+ * 
+ * @param string $user
+ * 
+ */
  public function setMediaUser($user)
  {
    $this->media_user = $user;
  }
 
+/**
+ * Set media access
+ * 
+ * @param mixed $string
+ * 
+ */
  public function setMediaAccess($access)
  {
    $this->media_access = $access;
  }
 
+/**
+ * Set media status
+ * 
+ * @param string $status
+ * 
+ */
  public function setMediaStatus($status)
  {
    $this->media_status = $status;
  }
 
+/**
+ * Grab all media
+ * retrieve all media records
+ * 
+ * @param integer $orderBy
+ * 
+ */
  public function grabAllMedia($orderBy = 'ID')
  {
    return $this->mediaDao->findAllMedia($orderBy);
  }
 
+/**
+ * GrabMedia
+ * retrieve single record of media
+ * 
+ * @param integer $id
+ * 
+ */
  public function grabMedia($id)
  {
    return $this->mediaDao->findMediaById($id, $this->sanitizer);
@@ -183,18 +229,53 @@ class MediaEvent
    $this->validator->sanitize($this->media_filename, 'string');
    $this->validator->sanitize($this->media_user, 'string');
    
+   return $this->mediaDao([
+     'media_filename' => $this->media_filename,
+     'media_caption' => $this->media_caption,
+     'media_type' => $this->media_type,
+     'media_target' => $this->media_target,
+     'media_user' => $this->media_user,
+     'media_access' => $this->media_access,
+     'media_status' => $this->media_status
+   ]);
   
-   
  }
 
  public function modifyMedia()
  {
 
+   $this->validator->sanitize($this->mediaId, 'int');
+   $this->validator->sanitize($this->media_caption, 'string');
+
+   if(empty($this->media_filename)) {
+
+      return $this->mediaDao->updateMedia($this->sanitizer, [
+        'media_caption' => $this->media_caption,
+        'media_target' => $this->media_target,
+        'media_access' => $this->media_access,
+        'media_status' => $this->media_status
+      ], $this->mediaId);
+
+   } else {
+
+      return $this->mediaDao->updateMedia($this->sanitizer, [
+         'media_filename' => $this->media_filename,
+         'media_caption' => $this->media_caption,
+         'media_access' => $this->media_access,
+         'media_status' => $this->media_status
+      ], $this->mediaId);
+
+   }
+
  }
 
  public function removeMedia()
  {
-   
+   $this->validator->sanitize($this->mediaId, 'int');
+
+   if(!$data_media = $this->mediaDao->findMediaById($this->mediaId, $this->sanitizer)) {
+      direct_page('index.php?load=media&error=mediaNotFound', 404);
+   }
  }
 
 }
