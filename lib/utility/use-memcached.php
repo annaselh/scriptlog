8e19339c79432ac46f7064b7906c92989f40a539
cakmoel <alanmoehammad@gmail.com>
2019-09-14 17:54:34 +0700

    update use_memcached function

--- lib/utility/use-memcached.php
+++ lib/utility/use-memcached.php
@@ -3,12 +3,13 @@
  * Function use_memcached
  * 
  * @category Function to use memcached class
+ * @package  SCRIPTLOG/LIB/UTILITY
  * 
  */
 function use_memcached($host)
 {
    
-    $var_cached = new Memcached();
+    $var_cached = new MemCached();
    
     return $var_cached -> addServer($host);
    
